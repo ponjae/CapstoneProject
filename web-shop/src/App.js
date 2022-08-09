8b2c36afefbf73c36480947eaabc5c61f3abafaa
ponjae <ponjae11@gmail.com>
2022-08-09 11:24:10 +0200

    added pre-chosen pictures

--- web-shop/src/App.js
+++ web-shop/src/App.js
@@ -5,22 +5,27 @@ const App = () => {
     {
       id: 1,
       title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
       title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
       title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
       title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
       title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
@@ -28,7 +33,12 @@ const App = () => {
     <div className="categories-container">
       {categories.map((category) => (
         <div className="category-container">
-          {/* <img className="background-img" /> */}
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${category.imageUrl})`,
+            }}
+          />
           <div className="category-body-container">
             <h2>{category.title}</h2>
             <p>Shop Now</p>
